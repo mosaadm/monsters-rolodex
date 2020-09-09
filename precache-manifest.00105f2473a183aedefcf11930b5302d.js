1f27166bbe86649d4f18188706dab4810d4e6ed1
mohamed mahmoud <mohamed.mahmoud@toptal.com>
2020-09-09 08:50:37 +0200

    Updates

---
+++ precache-manifest.00105f2473a183aedefcf11930b5302d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e43795bab97d850a2b7d88f81095b8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "95338b96e30b4271155d",
+    "url": "/monsters-rolodex/static/css/main.3146cbd7.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95338b96e30b4271155d",
+    "url": "/monsters-rolodex/static/js/main.8d170a4d.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
